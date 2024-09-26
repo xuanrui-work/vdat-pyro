a1a7e2ee11db4e7cdeaf777d17882a0f5b1154a3
None <None>
2024-09-26 14:05:18 -0400

    new bench model

--- bench_model/grad_rev.py
+++ bench_model/grad_rev.py
@@ -8,6 +8,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import numpy as np
+
+class GradientReversalF(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output.neg() * ctx.alpha, None
+
 class Model(nn.Module):
     def __init__(
         self,
@@ -22,17 +34,25 @@ class Model(nn.Module):
         self.hparams = hparams
 
         self.classifier = Classifier(in_shape, n_cls)
-    
+        self.domain_cls_head = nn.Linear(np.prod(self.classifier.cnn_block.out_shape), 2)
+
     def forward(self, x, d):
-        y1 = self.classifier(x, d)
+        x = self.classifier.foward_repr(x, d)
+        x = x.flatten(1)
+        y1 = self.classifier.op_cls(x)
+
+        x_rev = GradientReversalF.apply(x, self.hparams['alpha'])
+        d1 = self.domain_cls_head(x_rev)
+
         outputs = {
             'x': x,
-            'y1_lo': y1
+            'y1_lo': y1,
+            'd1_lo': d1
         }
         if d == 'src':
             outputs = {k+'_A': v for k, v in outputs.items()}
         elif d == 'tgt':
-            outputs = {k+'_B': v for k, v in outputs.items()} 
+            outputs = {k+'_B': v for k, v in outputs.items()}
         return outputs
 
 class Evaluator(EvalRunner):
@@ -56,12 +76,17 @@ class Evaluator(EvalRunner):
         acc_s = (outputs['y1_lo_A'].argmax(-1) == ys).sum() / N
         acc_t = (outputs['y1_lo_B'].argmax(-1) == yt).sum() / N
 
+        acc_ds = (outputs['d1_lo_A'].argmax(-1) == 0).sum() / N
+        acc_dt = (outputs['d1_lo_B'].argmax(-1) == 1).sum() / N
+
         loss_dict = {
             'loss': loss,
             'ce_s': loss_s,
             'ce_t': loss_t,
             'acc_s': acc_s,
-            'acc_t': acc_t
+            'acc_t': acc_t,
+            'acc_ds': acc_ds,
+            'acc_dt': acc_dt
         }
         return loss_dict
 
@@ -102,13 +127,22 @@ class Trainer(TrainRunner):
     def step(self, xs, ys, xt, yt):
         N = xs.shape[0]
 
+        alpha = 2 / (1 + np.exp(-10 * self.n_step/self.num_steps)) - 1
+        self.model.hparams['alpha'] = alpha
+
         outputs = {}
         outputs.update(self.model(xs, 'src'))
         outputs.update(self.model(xt, 'tgt'))
 
         loss_s = self.loss_fn(outputs['y1_lo_A'], ys)
         loss_t = self.loss_fn(outputs['y1_lo_B'], yt)
-        loss = loss_s
+
+        ds = torch.tensor([0]*N, device=xs.device)
+        dt = torch.tensor([1]*N, device=xt.device)
+        ld_s = self.loss_fn(outputs['d1_lo_A'], ds)
+        ld_t = self.loss_fn(outputs['d1_lo_B'], dt)
+
+        loss = loss_s + (ld_s + ld_t) / 2
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -117,11 +151,18 @@ class Trainer(TrainRunner):
         acc_s = (outputs['y1_lo_A'].argmax(-1) == ys).sum() / N
         acc_t = (outputs['y1_lo_B'].argmax(-1) == yt).sum() / N
 
+        acc_ds = (outputs['d1_lo_A'].argmax(-1) == 0).sum() / N
+        acc_dt = (outputs['d1_lo_B'].argmax(-1) == 1).sum() / N
+
         loss_dict = {
             'loss': loss,
             'ce_s': loss_s,
             'ce_t': loss_t,
             'acc_s': acc_s,
-            'acc_t': acc_t
+            'acc_t': acc_t,
+            'ld_s': ld_s,
+            'ld_t': ld_t,
+            'acc_ds': acc_ds,
+            'acc_dt': acc_dt
         }
         return loss_dict
